c4c3bd06fe9293b4a893768b623fa11dc983dd89
perriex <parna80as@gmail.com>
2021-08-13 16:05:36 +0430

    profile added

--- scripts/library.js
+++ scripts/library.js
@@ -1,97 +1,30 @@
-const profile_img = document.querySelector('.profile-photo_container');
-const profile_input = document.querySelector('.profile-photo input');
-const profile_label = document.querySelector('.profile-photo label');
-const profile_name = document.getElementById('profile-name');
-
-updateProfile();
-
-function setProfileName() {
-    profile_name.innerText = getUserName();
-}
-
-setProfileName();
-
-profile_input.addEventListener("change", function(event) {
-    let file = profile_input.files[0];
-    const modal_username = document.querySelector('.modal-getInfo input');
-    const modal_img = document.querySelector('.modal-image');
-    const delete_img = document.querySelector('#delete-modalImg');
-    const modal_input = document.querySelector('.modal-image input');
-    let photo_target = '../assets/Icons/user-profile.svg';
-    let username_target = getUserName();
-    const save_modal = document.getElementById('save-photo');
-    modal_username.value = getUserName();
-
-    modal_username.addEventListener('change', () => {
-        username_target = modal_username.value;
-    })
-    if (file) {
-        let reader = new FileReader();
-        reader.onload = (e) => {
-            modal_img.style.backgroundImage = `url('${e.target.result}')`;
-            photo_target = e.target.result;
-        }
-        reader.readAsDataURL(file);
-        delete_img.addEventListener('click', () => {
-            modal_img.style.backgroundImage = `url('../assets/Icons/user-profile.svg')`;
-        })
-
-        modal_input.addEventListener('change', (e) => {
-            let file_modal = modal_input.files[0];
-            if (file_modal) {
-                let reader_modal = new FileReader();
-                reader_modal.onload = (object) => {
-                    modal_img.style.backgroundImage = `url('${object.target.result}')`;
-                    photo_target = object.target.result;
-                }
-                reader_modal.readAsDataURL(file_modal);
-            }
-        })
-    }
-
-
-    save_modal.addEventListener('click', () => {
-        profile_img.style.backgroundImage = `url('${photo_target}')`;
-        setUserName(username_target);
-
-        let result_photo_link = `${photo_target}`;
-        PostData("postAlter", {
-                token: localStorage.getItem("token"),
-                username: username_target,
-                avatar: result_photo_link,
-            })
-            .then(res => {
-                updateProfile();
-            }, error => {
-            })
-
-        setProfileName();
-        document.getElementById('modal').style.display = 'none';
-    })
-
-
-}, false);
-
-profile_label.addEventListener('click', editProfile);
-
-const library_playlists = document.getElementById('library_playlists');
-
-const fillData_library = (playlists) => {
-        return `
+const library_playlists = document.getElementById("library_playlists");
+const fillData_library = (playlists, user_playlistsPics) => {
+  return `
   <div class="cards__container">
-      ${playlists.map(
-          (playlist) =>
+      ${playlists
+        .map(
+          (playlist, index) =>
             ` 
           <div class="card-playlist" >
-              <a id=${playlist.id} class="music-link" href="./songsList.html?id&playlist=${playlist.id}" >
+              <a id=${
+                playlist.id
+              } class="music-link" href="./songsList.html?id&playlist=${
+              playlist.id
+            }" >
                   <div class="music-link__image">
-                  <img src="${playlist.cover?playlist.cover:'../assets/Icons/musical-note.svg'}" alt="cover" />
+                  <img src="${
+                    user_playlistsPics[index] &&
+                    user_playlistsPics[index] !== ""
+                      ? user_playlistsPics[index]
+                      : "../assets/Icons/musical-note.svg"
+                  }" alt="cover" />
                   <button class="play-playlists-button">
                   <img src="../assets/Icons/play-button.svg"  class="play_sign_button svgColor" alt="">
                   </button>
                   </div>
                   <h4 class="card-playlist__title">${playlist.name}</h4>
-                  <p>${localStorage.getItem('username')}</p>
+                  <p>${localStorage.getItem("username")}</p>
               </a>
           </div>
       `
@@ -99,52 +32,74 @@ const fillData_library = (playlists) => {
         .join("")}
   </div>
 
-`
-
+`;
 };
-
-
 
 /***************** work with playlists ******************** */
 
-
-function getAllPlaylists(){
-    const user_playlists = [];
-    PostData("postAllPlaylists", {
-        token: localStorage.getItem("token")
-    }).then(res => {
-        res.forEach(item => {
-            user_playlists.push(item);
-            getPlaylistSongs(item['id']);
-        });
-        library_playlists.innerHTML = fillData_library(user_playlists);
-    }).catch(error=>{
-       showToast(error);
+async function getAllPlaylists() {
+  const user_playlists = [];
+  const user_playlistsPics = [];
+  PostData("postAllPlaylists", {
+    token: getToken(),
+  })
+    .then((res) => {
+      res.forEach((item) => {
+        user_playlists.push(item);
+        item.songs[0]
+          ? user_playlistsPics.push(item.songs[0].rest.cover)
+          : user_playlistsPics.push("");
+      });
+      library_playlists.innerHTML = fillData_library(
+        user_playlists,
+        user_playlistsPics
+      );
+    })
+    .catch((error) => {
+      showToast(error);
     });
 }
 
-getAllPlaylists();
+await getAllPlaylists();
 
-let avatar_img;
-
-function updateProfile(){
-    GetData("getUser", parseInt(localStorage.getItem('userId')) )
-    .then(res => {
-        avatar_img = res['user']['avatar'];
-        let image_url = document.querySelector('.profile-photo_container').style.backgroundImage;
-        document.querySelector('.profile-photo_container').style.backgroundImage = `url(${avatar_img})`;
-        // profile_img.style.backgroundImage = avatar;
-    })
+/**** Edit profile ****/
+const profilePicContainer = document.querySelector(".profile-photo_container");
+function getUserData() {
+  let userId = localStorage.getItem("userId");
+  GetData("getUser", userId).then((res) => {
+    document.getElementById("profile-name").innerHTML =
+      res.user.first_name + " " + res.user.last_name;
+    document.getElementById("profile-username").innerHTML = res.user.username;
+    if (res.user.avatar)
+      profilePicContainer.style.backgroundImage = `url(${res.user.avatar})`;
+  });
 }
 
-// function getUserData(){
-//     const data;
-//     GetData("getUser", parseInt(localStorage.getItem('userId')) )
-//     .then(res => {
-//         data = res;
-//         avatar = res['user']['avatar'];
-//         profile_img.style.backgroundImage = `url(${avatar})`;
-//         // profile_img.style.backgroundImage = avatar;
-//     })
-//     return data;
-// }
+getUserData();
+
+/**** profile uploader****/
+const profile_input = document.querySelector(".profile-photo input");
+
+profile_input.addEventListener(
+  "change",
+  function (event) {
+    let file = profile_input.files[0];
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    if(file.size/1000 < 85){
+    reader.onload = function () {
+      PostData("postAlter", {
+        token: localStorage.getItem("token"),
+        avatar: reader.result,
+      }).then(
+        (res) => {
+          getUserData()},
+        (error) => {}
+      );
+    };
+  }else{
+    showToast("حجم فایل زیاد است")
+  }
+  },
+  false
+);
