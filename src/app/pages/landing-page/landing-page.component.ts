6cf130ad044b03b8e303c492ad27a1c16e475802
perriex <parna80as@gmail.com>
2021-08-16 21:06:42 +0430

    add link to cards

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,19 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import Song from 'src/app/models/SongModal';
-import { EngineService } from 'src/app/service/engine.service';
+import { Component, HostListener, OnInit } from "@angular/core";
+import Song from "src/app/models/SongModal";
+import { EngineService } from "src/app/service/engine.service";
 
 @Component({
-  selector: 'app-landing-page',
-  templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.scss']
+  selector: "app-landing-page",
+  templateUrl: "./landing-page.component.html",
+  styleUrls: ["./landing-page.component.scss"],
 })
 export class LandingPageComponent implements OnInit {
-public songs : Song[] = [];
-  
-public constructor(private engineService: EngineService) {}
+  public songs: Song[] = [];
+  public constructor(private engineService: EngineService) {}
 
-public async ngOnInit() {
+  public async ngOnInit() {
     this.songs = await this.engineService.getAllSongs();
-}
-  
+  }
 }
