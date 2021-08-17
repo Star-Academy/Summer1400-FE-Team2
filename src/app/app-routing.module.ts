e256f809969c9c7e5276abfe185fca4f0a37903e
perriex <parna80as@gmail.com>
2021-08-17 16:32:42 +0430

    add all-song to app route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { LayoutComponent } from "./layout/layout.component";
+import { AllSongsComponent } from "./pages/all-songs/all-songs.component";
 import { LandingPageComponent } from "./pages/landing-page/landing-page.component";
 import { LibraryComponent } from "./pages/library/library.component";
 import { LoginComponent } from "./pages/login/login.component";
@@ -16,6 +17,11 @@ const routes: Routes = [
     path: "library",
     component: LayoutComponent,
     children: [{ path: "", component: LibraryComponent }],
+  },
+  {
+    path: "all-songs",
+    component: LayoutComponent,
+    children: [{ path: "**", component: AllSongsComponent }],
   },
   { path: "login", component: LoginComponent },
 ];
