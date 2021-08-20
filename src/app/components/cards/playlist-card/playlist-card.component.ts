5a9efa38ee60bdc85b1e24fbefda838ca76ccc50
hadis ghafouri <hadisghafouri98@gmail.com>
2021-08-20 17:16:53 +0430

    edit button to card-playlist

--- src/app/components/cards/playlist-card/playlist-card.component.ts
+++ src/app/components/cards/playlist-card/playlist-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from "@angular/core";
 import Playlist from "src/app/models/Playlist";
+import { EngineService } from "src/app/service/engine.service";
 
 @Component({
   selector: "app-playlist-card",
@@ -7,10 +8,15 @@ import Playlist from "src/app/models/Playlist";
   styleUrls: ["./playlist-card.component.scss"],
 })
 export class PlaylistCardComponent implements OnInit {
+  constructor(private engine:EngineService) {}
+  ngOnInit(): void {}
+
   @Input() public playlist!: Playlist;
   default_img = "../../../assets/Icons/musical-note.svg";
+  default_artist =this.engine.getUsername();
 
-  constructor() {}
+  onEditBtn(){
+    console.log('on edit button');
+  }
 
-  ngOnInit(): void {}
 }
