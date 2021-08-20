a3156eb78e60868a9c194ac7f674453c3752d44e
perriex <parna80as@gmail.com>
2021-08-20 23:52:04 +0430

    sync current song

--- src/app/components/current-music/current-music.component.ts
+++ src/app/components/current-music/current-music.component.ts
@@ -1,4 +1,6 @@
+import { Location } from "@angular/common";
 import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 import Song from "src/app/models/SongModal";
 import { PlayerService } from "../player/player.service";
 
@@ -8,16 +10,77 @@ import { PlayerService } from "../player/player.service";
   styleUrls: ["./current-music.component.scss"],
 })
 export class CurrentMusicComponent implements OnInit {
-  constructor(public player: PlayerService) {}
+  constructor(
+    private _Activatedroute: ActivatedRoute,
+    public player: PlayerService,
+    private location: Location
+  ) {}
 
   public id: number | null = 1;
   public song: Song | null = null;
-  public isPlaying: Boolean = false;
+
+  public linkPhotoPlay: string = "../assets/Icons/play-button.svg";
+  public linkPhotoShuffle: string =
+    "../assets/Icons/shuffle-disabled-button.svg";
+  public linkPhotoReplay: string = "../assets/Icons/loop-button.svg";
+
   public async ngOnInit() {
     await this.player.getAllSongs();
     let number = this.player.getId();
     if (number) {
       this.song = this.player.getSong();
     }
+    if (this.player.autoPlay)
+      this.linkPhotoPlay = "../assets/Icons/pause-button.svg";
+    if (this.player.isShuffle)
+      this.linkPhotoShuffle = "../assets/Icons/shuffle-button.svg";
+    if (this.player.isLoop)
+      this.linkPhotoReplay = "../assets/Icons/right-arrow-button.svg";
+    let progress = document.getElementById("progress");
+    console.log(progress);
+    setInterval(() => {
+      if (progress) {
+        progress.style.width = this.player.getProgress() + "%";
+      }
+    }, 500);
+  }
+
+  public playSong() {
+    if (!this.player.autoPlay) {
+      this.player.playSong();
+      this.linkPhotoPlay = "../assets/Icons/pause-button.svg";
+    } else {
+      this.player.pauseSong();
+      this.linkPhotoPlay = "../assets/Icons/play-button.svg";
+    }
+  }
+
+  public nextSong() {
+    this.player.getNextSong();
+    this.song = this.player.getSong();
+    this.id = this.player.getId();
+  }
+
+  public prevSong() {
+    this.player.getPrevSong();
+    this.song = this.player.getSong();
+    this.id = this.player.getId();
+  }
+  public replaySong() {
+    if (!this.player.isLoop) {
+      this.linkPhotoReplay = "../assets/Icons/right-arrow-button.svg";
+    } else {
+      this.linkPhotoReplay = "../assets/Icons/loop-button.svg";
+    }
+    this.player.replaySong();
+  }
+
+  public shuffleSongs() {
+    if (!this.player.isShuffle) {
+      this.linkPhotoShuffle = "../assets/Icons/shuffle-button.svg";
+    } else {
+      this.linkPhotoShuffle = "../assets/Icons/shuffle-disabled-button.svg";
+    }
+    this.player.shuffleSong();
   }
 }
--- src/app/pages/music-player/music-player.component.ts
+++ src/app/pages/music-player/music-player.component.ts
@@ -1,3 +1,4 @@
+import { Location } from "@angular/common";
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { PlayerService } from "src/app/components/player/player.service";
@@ -11,14 +12,12 @@ import Song from "src/app/models/SongModal";
 export class MusicPlayerComponent implements OnInit {
   constructor(
     private _Activatedroute: ActivatedRoute,
-    public player: PlayerService
+    public player: PlayerService,
+    private location: Location
   ) {}
 
   public id: number | null = 1;
   public song: Song | null = null;
-  private isPlaying: Boolean = false;
-  private isLoop: Boolean = false;
-  private isShuffle: Boolean = false;
   public duration: string = "00:00";
   public currentTime: string = "00:00";
 
@@ -36,9 +35,6 @@ export class MusicPlayerComponent implements OnInit {
         let num = parseInt(number);
         if (!this.player.autoPlay || num !== this.player.id) {
           this.player.setId(num);
-          this.isPlaying = this.player.autoPlay;
-          this.isLoop = this.player.isLoop;
-          this.isShuffle = this.player.isShuffle;
         }
         this.song = this.player.getSong();
         if (this.player.autoPlay)
@@ -48,8 +44,12 @@ export class MusicPlayerComponent implements OnInit {
         if (this.player.isLoop)
           this.linkPhotoReplay = "../assets/Icons/right-arrow-button.svg";
         this.getDuration();
+        let progress = document.getElementById("progress");
         setInterval(() => {
           this.currentTime = this.player.getCurrentTime();
+          if (progress) {
+            progress.style.width = this.player.getProgress() + "%";
+          }
         }, 500);
       }
     });
@@ -58,24 +58,23 @@ export class MusicPlayerComponent implements OnInit {
     window.history.back();
   }
   public playSong() {
-    if (!this.isPlaying) {
+    if (!this.player.autoPlay) {
       this.player.playSong();
       this.linkPhotoPlay = "../assets/Icons/pause-button.svg";
     } else {
       this.player.pauseSong();
       this.linkPhotoPlay = "../assets/Icons/play-button.svg";
     }
-    this.isPlaying = !this.isPlaying;
   }
 
   private getDuration() {
+    this.location.replaceState("/all-songs/" + this.id);
     let timer = setInterval(() => {
       let time = this.player.getTimeDuration();
       if (time !== "00:00") {
         this.duration = time;
         clearInterval(timer);
       }
-      console.log("call");
     }, 100);
   }
   public nextSong() {
@@ -90,26 +89,23 @@ export class MusicPlayerComponent implements OnInit {
     this.song = this.player.getSong();
     this.id = this.player.getId();
     this.getDuration();
-    console.log("change");
   }
 
   public replaySong() {
-    if (!this.isLoop) {
+    if (!this.player.isLoop) {
       this.linkPhotoReplay = "../assets/Icons/right-arrow-button.svg";
     } else {
       this.linkPhotoReplay = "../assets/Icons/loop-button.svg";
     }
     this.player.replaySong();
-    this.isLoop = !this.isLoop;
   }
 
   public shuffleSongs() {
-    if (!this.isShuffle) {
+    if (!this.player.isShuffle) {
       this.linkPhotoShuffle = "../assets/Icons/shuffle-button.svg";
     } else {
       this.linkPhotoShuffle = "../assets/Icons/shuffle-disabled-button.svg";
     }
     this.player.shuffleSong();
-    this.isShuffle = !this.isShuffle;
   }
 }
