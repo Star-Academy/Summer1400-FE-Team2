b4732dfea5ace019da171edd0d2b56b607277337
hadis ghafouri <hadisghafouri98@gmail.com>
2021-09-18 11:17:00 +0430

    add delete playlist modal

---
+++ src/app/components/modals/delete-playlist/delete-playlist.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit ,Inject, Optional} from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
+
+import { PlaylistCardComponent } from '../../cards/playlist-card/playlist-card.component';
+
+@Component({
+  selector: 'app-delete-playlist',
+  templateUrl: './delete-playlist.component.html',
+  styleUrls: ['./delete-playlist.component.scss']
+})
+export class DeletePlaylistComponent implements OnInit {
+
+  constructor(
+    public _dialog: MatDialog,
+    public dialogRef: MatDialogRef<PlaylistCardComponent>,
+    @Optional() @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
+
+  ngOnInit(): void {
+  }
+}
